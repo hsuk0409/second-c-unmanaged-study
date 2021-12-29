44f5a4d18ea81b2607bcdf6de0e331dbc7a6682a
chionsuk <hsuk0409@gmail.com>
2021-12-29 23:09:24 +0900

    문자열 복습

--- c-unmaged-code/adaptation/Main.c
+++ c-unmaged-code/adaptation/Main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "bubble_sort.h"
 #include "quick_sort.h"
+#include "print_string.h"
 
 #define ARR_SIZE (10)
 
@@ -10,17 +11,25 @@ int main(void)
 {
     int nums1[ARR_SIZE] = {9, 2, 4, 6, 5, 1, 10, 3, 7, 8};
     int nums2[ARR_SIZE] = {8, 2, 5, 4, 6, 3, 12, 0, 10, 1};
+    char* str = "Hi, i'm justin!!";
 
+    print_string(str);
+    printf("%c\n", str[0]);
+
+    printf("\n");
     printf("Before bubble sort!\n");
     printArr(nums1);
 
+    printf("\n");
     printf("After bubble sort!\n");
     bubble_sort(nums1, ARR_SIZE);
     printArr(nums1);
 
+    printf("\n");
     printf("Before quick sort!\n");
     printArr(nums2);
 
+    printf("\n");
     printf("After quick sort!\n");
     my_quick_sort(nums2, 0, (size_t)(ARR_SIZE - 1));
     printArr(nums2);
---
+++ c-unmaged-code/adaptation/print_string.h
@@ -0,0 +1,13 @@
+#ifndef PRINT_STRING_H
+#define PRINT_STRING_H
+
+#include <stdio.h>
+
+void print_string(char* str);
+
+void print_string(char* str)
+{
+    printf("%s\n", str);
+}
+
+#endif
