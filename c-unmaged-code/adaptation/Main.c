7156b6aacb621c520f698e930a09eee0f7410ac3
chionsuk <hsuk0409@gmail.com>
2022-01-02 23:10:39 +0900

    char test

--- c-unmaged-code/adaptation/Main.c
+++ c-unmaged-code/adaptation/Main.c
@@ -12,6 +12,12 @@ int main(void)
     int nums1[ARR_SIZE] = {9, 2, 4, 6, 5, 1, 10, 3, 7, 8};
     int nums2[ARR_SIZE] = {8, 2, 5, 4, 6, 3, 12, 0, 10, 1};
     char* str = "Hi, i'm justin!!";
+    char ch_val = 'A';
+
+    printf("A char value: %c\n", ch_val);
+    printf("A int value: %d\n", ch_val);
+    printf("B char value: %c\n", ch_val + 1);
+    printf("\n");
 
     print_string(str);
     printf("%c\n", str[0]);
