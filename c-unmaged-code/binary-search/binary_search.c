31f7259156f3377faa010f124e4f42ec60ee6634
chionsuk <hsuk0409@gmail.com>
2022-01-10 22:53:08 +0900

    인자로 받은 배열의 주소 출력해보기

--- c-unmaged-code/binary-search/binary_search.c
+++ c-unmaged-code/binary-search/binary_search.c
@@ -8,6 +8,10 @@ int binary_search(int* nums, size_t num_len, int target)
     int end = (int)num_len;
     int mid = (start + end) / 2;
     
+    printf("First value of nums: %d\n", nums[0]);
+    printf("Address of nums: %x\n", nums);
+    printf("Address of next num_p: %x\n", ++num_p);
+
     while (start <= end) {
         printf("num[mid]: %d\n", num_p[mid]);
 
