0d3bd89e8cec9ae662a9b31c6c3dc17379bdd81a
chionsuk <hsuk0409@gmail.com>
2022-01-12 22:09:40 +0900

    2차원 배열 초기화 테스트

--- c-unmaged-code/array/main.c
+++ c-unmaged-code/array/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+static int arrArr[20][20];
+
+void set_arr(void);
+
 int main(void)
 {
-    int arrArr[20][20] = {0,};
+    
     size_t i;
     size_t k;
 
+    set_arr();
     for (i = 0; i < 20; ++i) {
+        printf("%lu= ", i);
         for (k = 0; k < 20; ++k) {
             printf("%d ", arrArr[i][k]);
         }
@@ -14,4 +20,21 @@ int main(void)
     }
 
     return 0;
+}
+
+void set_arr(void)
+{
+    size_t i, k;
+
+    for (i = 0; i < 15; ++i) {
+        for (k = 0; k < 15; ++k) {
+            arrArr[i][k] = 0;
+        }
+    }
+
+    for (i = 15; i < 20; ++i) {
+        for (k = 0; k < 20; ++k) {
+            arrArr[i][k] = 3;
+        }
+    }
 }
