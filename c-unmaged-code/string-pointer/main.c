8dd293948c7064ba353f981dd7f21640c7e59fb5
chionsuk <hsuk0409@gmail.com>
2022-01-20 23:01:38 +0900

    문자열 조작 수정

--- c-unmaged-code/string-pointer/main.c
+++ c-unmaged-code/string-pointer/main.c
@@ -5,8 +5,10 @@ void reverse_string(char* str);
 int main(void)
 {
     char* tmp_str = "Hi, my name is justin!";
-    reverse_string(tmp_str);
-    printf("Reversed string: %s\n", tmp_str);
+    char tmp_str2[25] = "Hi, my name is justin!";
+    reverse_string(tmp_str2);
+    printf("111 Reversed string: %s\n", tmp_str);
+    printf("222 Reversed string: %s\n", tmp_str2);
 
     return 0;
 }
@@ -15,22 +17,26 @@ void reverse_string(char* str)
 {
     char* start = str;
     char* end = str;
+    char tmp;
 
     while (*end != '\0') {
+        printf("%c ", *end);
         ++end;
     }
     --end;
 
     printf("\n");
-    printf("String: %s\n", str);
-    printf("Start of string: %c\n", *start);
     printf("End of string: %c\n", *end);
+    printf("Is equal start to end? %d\n", start == end);
 
-    printf("Address of start: %s\n", start);
-    printf("Address of end: %c\n", *(--end));
-
-    while (&start != &end) {
-        printf("%c ", (*end)--);
+    while (start != end) {
+        tmp = *start;
+        *end = *start;
+        *start = tmp;
+        printf("%s\n", str);
+        if (start == end) break;
+        ++start;
+        --end;
     }
-    printf("\n");
+    printf("Final tmp_str:: %s\n", str);
 }
