896059cca389ae76897271f375a84e840ebf0539
chionsuk <hsuk0409@gmail.com>
2022-02-04 22:55:57 +0900

    파일 열어서 내용 작성해보기

--- c-unmaged-code/file-io/main.c
+++ c-unmaged-code/file-io/main.c
@@ -1,6 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
+    FILE *fp = fopen("hello.txt", "w");
+
+    fputs("Hello, justin!\n", fp);
+    fputs("Bye, justin!", fp);
+
+    fclose(fp);
+
     return 0;
 }
