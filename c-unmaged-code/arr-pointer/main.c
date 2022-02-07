34f99a6a030f3aa41e3465dde8311228d904cd7f
chionsuk <hsuk0409@gmail.com>
2022-02-07 12:03:43 +0900

    2차원 배열 인자로 받기

--- c-unmaged-code/arr-pointer/main.c
+++ c-unmaged-code/arr-pointer/main.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 
+void print_arr(const char arr[][10]);
+
 int main(void)
 {
-    char chars[50];
-    const char* chars_p[3];
-    size_t i;
+    {
+        char chars[50];
+        const char* chars_p[3];
+        size_t i;
 
-    chars_p[0] = &chars[8];
-    chars_p[1] = &chars[33];
+        chars_p[0] = &chars[8];
+        chars_p[1] = &chars[33];
 
-    printf("chars_p[0].. ");
-    for (i = 0; i < sizeof(chars_p[0]); ++i) {
-        if (chars_p[0][i] != '\0') {
-            printf("i:: %lu ", i);
+        printf("chars_p[0].. ");
+        for (i = 0; i < sizeof(chars_p[0]); ++i) {
+            if (chars_p[0][i] != '\0') {
+                printf("i:: %lu ", i);
+            }
         }
-    }
-    printf("\n");
+        printf("\n");
 
-    printf("chars_p[1].. ");
-    for (i = 0; i < sizeof(chars_p[1]); ++i) {
-        if (chars_p[1][i] != '\0') {
-            printf("i:: %lu ", i);
+        printf("chars_p[1].. ");
+        for (i = 0; i < sizeof(chars_p[1]); ++i) {
+            if (chars_p[1][i] != '\0') {
+                printf("i:: %lu ", i);
+            }
         }
+        printf("\n");
     }
-    printf("\n");
+
+    {
+        char arr[2][10] = {
+            "Name: ",
+            "Justin!"
+        };
+
+        print_arr(arr);
+    }
 
     return 0;
+}
+
+void print_arr(const char arr[][10])
+{
+    printf("First chars:: %s\n", arr[0]);
+    printf("Second chars:: %s\n", arr[1]);
 }
